b9858f1b61ddac2e5f0429da47d6b57d164daf2b
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2025-05-08 19:47:28 +0530

    new commit

--- src/partials/Post.js
+++ src/partials/Post.js
@@ -1,9 +1,7 @@
 import {formatISO9075} from "date-fns";
 import {Link} from "react-router-dom";
 import "../css/postpage.css";
-
 export default function Post({_id,title,summary,cover,content,createdAt,author}) {
-
   return (
     <div className="post">
       <div className="image">
